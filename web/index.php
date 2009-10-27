f13f3abeb2c70f14e059f90d08ef88af07d51395
Daniel Challen <daniel.challen@gmail.com>
2009-10-27 14:34:24 +0000

    first git push of all files

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+# Copyright 2006 Ohio Supercomputer Center
+# Revision info:
+# $HeadURL: http://svn.osc.edu/repos/pbstools/releases/pbstools-2.0/web/index.php $
+# $Revision: 93 $
+# $Date: 2006-02-15 13:53:25 -0500 (Wed, 15 Feb 2006) $
+require_once 'page-layout.php';
+
+page_header("");
+page_footer();
+?>
